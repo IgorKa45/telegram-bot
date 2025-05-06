2a58d9ab14f4791dda15ea9a877eddffdf97efc1
пк <igorkanasnik@gmail.com>
2025-05-06 11:50:46 +0400

    Задача 1. Сборка телеграм-бота

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,21 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+
+            // Проверяем, что апдейт содержит сообщение и текст
+            if (update.message() !=null && update.message().text() != null) {
+                String messageText = update.message().text();
+                Long chatId = update.message().chat().id();
+
+                // Обрабатываем команду /start
+                if (messageText.equals("/start")) {
+                    String startMessege = "Бот запущен. Привет! Я бот. Вот что я могу(пока мало что)" +
+                            "1.Напоминать тебе о событиях которые ты хочешь не забыть. (Пиши в формате: 01.01.2022 20:00 Сделать домашнюю работу)";
+                    //Отправляем сообщение
+                    SendMessage message = new SendMessage(chatId, startMessege);
+                    telegramBot.execute(message);
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
